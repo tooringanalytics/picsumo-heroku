7024ddb768019a7de57c1abeffbd82d0d43a807f
Ryan Luedecke <ryan@sumojerky.com>
2015-03-11 07:47:53 -0500

    update

--- api/controllers/UploadController.js
+++ api/controllers/UploadController.js
@@ -7,8 +7,8 @@
 var AWS = require('aws-sdk');
 
 var s3  = new AWS.S3({
-    accessKeyId: 'AKIAJCA7CFBEBQQ6KUDA',
-    secretAccessKey: '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'
+    accessKeyId: '',
+    secretAccessKey: ''
 });
 
 module.exports = {
