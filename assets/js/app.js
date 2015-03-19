c0655b7b5e847f8271911b89b916e3cd96545904
Anshuman K <tooringtest@gmail.com>
2015-03-19 20:12:07 +0000

    Merged with the picsumo app

---
+++ assets/js/app.js
@@ -0,0 +1,44 @@
+angular.module('app', ['app.controllers', 'ui.router'])
+.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+  // $locationProvider.html5Mode(true);
+
+  $stateProvider
+  .state('home', {
+    url: '/',
+    templateUrl: 'templates/home.html',
+    controller: 'HomeCtrl'
+  })
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl'
+  })
+  .state('before', {
+    url: '/before',
+    templateUrl: 'templates/before.html',
+    controller: 'BeforeCtrl'
+  })
+  .state('fake', {
+  url: '/fake',
+  templateUrl: 'templates/fake.html',
+  controller: 'FakeCtrl'
+  })
+  .state('after', {
+    url: '/after',
+    templateUrl: 'templates/after.html',
+    controller: 'AfterCtrl'
+  })
+  .state('share', {
+    url: '/share',
+    templateUrl: 'templates/share.html',
+    controller: 'ShareCtrl'
+  })
+  .state('gallery', {
+    url: '/gallery',
+    templateUrl: 'templates/gallery.html',
+    controller: 'GalleryCtrl'
+  });
+
+  $urlRouterProvider.otherwise('/');
+});
--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -22,12 +22,19 @@ var cssFilesToInject = [
 // Client-side javascript files to inject in order
 // (uses Grunt-style wildcard/glob/splat expressions)
 var jsFilesToInject = [
-  
+
   // Load sails.io before everything else
   'js/dependencies/sails.io.js',
 
   // Dependencies like jQuery, or Angular are brought in here
-  'js/dependencies/**/*.js',
+  //'js/dependencies/**/*.js',
+  'js/dependencies/angular.js',
+  'js/dependencies/angular-ui-router.js',
+  'js/dependencies/ng-file-upload.js',
+  'js/dependencies/ng-file-uploads-shims.js',
+  'js/dependencies/ng-mocks.js',
+  'js/dependencies/validator.js',
+  'js/dependencies/webcam.js',
 
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
