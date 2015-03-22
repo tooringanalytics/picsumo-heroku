75d06f03d0e9cfd74da91129761ba8812ca99bcd
Anshuman K <tooringtest@gmail.com>
2015-03-22 02:15:23 +0000

    heroku build experiment

--- tasks/register/heroku.js
+++ tasks/register/heroku.js
@@ -1,10 +1,14 @@
 module.exports = function (grunt) {
     grunt.registerTask('heroku:production', [
         'compileAssetsProd',
-        'copy:dev',
-        'coffee:dev',
         'concat',
         'uglify',
-        'cssmin'
+        'cssmin',
+        'sails-linker:prodJs',
+        'sails-linker:prodStyles',
+        'sails-linker:devTpl',
+        'sails-linker:prodJsJade',
+        'sails-linker:prodStylesJade',
+        'sails-linker:devTplJade'
     ]);
 };
