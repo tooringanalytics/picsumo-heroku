412a7ef206b3b86f6e9009064249a7d0ee20cb00
Anshuman K <tooringtest@gmail.com>
2015-03-28 19:08:47 +0000

    Fixed bug in DailyTasks

--- crontab/DailyTasks.js
+++ crontab/DailyTasks.js
@@ -1,9 +1,9 @@
 module.exports = {
     run : function () {
-        console.log('do something very cool here: ' + Date.now().toISOString());
+        console.log('do something very cool here: ' + Date.now());
     },
 
     sendReminderEmails : function () {
-        console.log('Time to send reminder emails...' + Date.now().toISOString());
+        console.log('Time to send reminder emails...' + Date.now());
     }
 };
