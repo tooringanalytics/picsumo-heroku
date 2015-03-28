29afff5e921dad62270f1c3cadba003f6ae3b985
Anshuman K <tooringtest@gmail.com>
2015-03-28 19:28:35 +0000

    Fixed bug in DailyTasks

--- crontab/DailyTasks.js
+++ crontab/DailyTasks.js
@@ -1,9 +1,9 @@
 module.exports = {
     run : function () {
-        sails.debug.log('do something very cool here: ' + Date.now().toISOString());
+        sails.log.debug('do something very cool here: ' + Date.now().toISOString());
     },
 
     sendReminderEmails : function () {
-        sails.debug.log('Time to send reminder emails...' + Date.now().toISOString());
+        sails.log.debug('Time to send reminder emails...' + Date.now().toISOString());
     }
 };
